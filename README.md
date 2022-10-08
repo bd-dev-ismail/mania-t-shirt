66206f5f1cc117fa15e1f3f6a99e638b3ec1a369
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-08 14:41:15 +0600

    added conditional rendering

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Module 54 project: t-shirt-mania ..before assignment 9
+
+## Conditional Reanderning
+- use element in a variabel using if else!
+- using ternary operator ? true : false!
+- using && operator! (is true)
+- using || operator! (is false)
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({ cart, removeItem }) => {
+  let messenge;
+  if(cart.length === 0){
+    messenge = <p>There are no Products Added yet</p>
+  }
+  else if(cart.length === 1){
+    messenge = <p>R o ekta kino amr jonno! </p>
+  }
+  else{
+    messenge = <p>Thanks! for buy something form us!</p>
+  }
   return (
-    <div>
-      <h3>Order Summary</h3>
+    <div className={cart.length === 0 ? `orange` : `blue`}>
+      <h3 className={`bold ${cart.length ===2 ? 'purple' : 'yellow'}`}>Order Summary</h3>
       <h5>Order Quantity {cart.length}</h5>
       {cart.map((tshirt) => (
         <p key={tshirt._id}>
@@ -11,6 +21,12 @@ const Cart = ({ cart, removeItem }) => {
           <button onClick={() => removeItem(tshirt)}>X</button>
         </p>
       ))}
+      {messenge}
+      {cart.length === 3 ? <p>You added three Item</p> : <p>Added More Item</p>}
+      <p>&& Operator</p>
+      {cart.length === 2 && <h2>You added Two Item</h2>}
+      <p>|| operator</p>
+      {cart.length === 4 || <p>You Not added Four Item</p>}
     </div>
   );
 };
